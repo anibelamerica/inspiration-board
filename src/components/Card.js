6e015089a3b6c45a64512c181b4b08643aedaf07
Anibel America <ameriabel@gmail.com>
2018-12-13 15:18:06 -0800

    adds preliminary Card component content - needs emoji install

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,27 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+    //   text: this.props.card.text,
+    //   emoji: this.props.card.emoji
+    // };
+    //
+    // console.log(this.state);
+    // console.log(props);
+    // console.log(props);
+  }
+
   render() {
+    const { text, emoji } = this.props.card;
     return (
-      <div className="card">
-        Card
+      <div className="card card__content">
+        { text &&
+          <h1 className='card__content-text'>{text}</h1>}
+        { emoji &&
+          <div className='card__content-emoji'>{emoji}</div>}
       </div>
     )
   }
