fb894922d7ec29270b6ca82cdf0be2c54507672b
Anibel America <ameriabel@gmail.com>
2018-12-14 16:47:15 -0800

    fixes merge conflicts

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,14 +5,13 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // cards: CARD_DATA.cards,
       url: this.props.url,
       boardName: this.props.boardName,
       cards: []
@@ -37,6 +36,49 @@ class Board extends Component {
     });
   }
 
+  deleteCard = (cardId) => {
+    console.log(cardId);
+    console.log("I'm in the delete card method");
+    const deleteURL = "https://inspiration-board.herokuapp.com/cards/" + cardId;
+
+    axios.delete(deleteURL)
+    .then((response) => {
+      const newCards = [...this.state.cards];
+      const index = newCards.findIndex(content => content.card.id === cardId);
+      newCards.splice(index, 1);
+
+      this.setState({
+        cards: newCards
+      });
+    })
+    .catch((error) => {
+      this.setState({
+        error: error.message
+      });
+    });
+  };
+
+  addCard = (cardData) => {
+    const POST_INSPO_CARDS_URL = this.props.url + '/' + this.props.boardName + '/cards';
+    axios.post(POST_INSPO_CARDS_URL, cardData)
+    .then((response) => {
+      cardData.id = response.data.card.id;
+
+      const updatedCardDeck = [...this.state.cards, {card: cardData}];
+
+      this.setState({
+        cards: updatedCardDeck
+      })
+
+    })
+    .catch((error) => {
+      this.setState({
+        error: error.message
+      });
+    });
+
+  };
+
   render() {
 
     const cardList = this.state.cards.map((cardContainer, i) => {
@@ -45,8 +87,13 @@ class Board extends Component {
     });
 
     return (
-      <div className="board">
-        { cardList }
+      <div>
+        <div className="board">
+          { cardList }
+
+          <NewCardForm
+            addCardCallback={this.addCard}/>
+        </div>
       </div>
     )
   }
--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -4,3 +4,112 @@ import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+
+class NewCardForm extends Component{
+
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+      emoji: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    const newState = {};
+
+    newState[event.target.name] = event.target.value;
+
+    this.setState(newState);
+    console.log(this.state);
+  }
+
+  getFormFields = () => {
+    return FIELDS.map((field, i) => {
+      return (
+        <div key={i}>
+          <div>
+              <label htmlFor={field} className="new-card-form__form-label">
+                <h1>
+                  {field}
+                </h1>
+              </label>
+          </div>
+          <div>
+            <textarea
+              name={field}
+              type="textarea"
+              value={this.state[field]}
+              onChange={this.onInputChange}
+              className="new-card-form__form-textarea"/>
+          </div>
+        </div>
+
+      )
+    })
+  };
+
+  resetState = () => {
+    this.setState({
+      text: '',
+      emoji: ''
+    });
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    const { text, emoji} = this.state;
+
+    this.props.addCardCallback(this.state);
+    this.resetState();
+  }
+
+//   getFields() {
+//   const exampleFormat = this.props.fields.map((field) => {
+//     if (field.key) {
+//       return <input
+//         placeholder={field.placeholder}
+//         name={field.key}
+//         type="text"
+//         value={this.state[field.key]}
+//         onChange={this.onInputChange}
+//         className={this.inputValid(field.key)}/>
+//
+//     } else {
+//       return field;
+//     }
+//   });
+//   return exampleFormat;
+// }
+
+  render() {
+    return (
+      <section className="new-card-form">
+        <section className="new-card-form__header">
+          <h1>Send an Inspiration!</h1>
+        </section>
+        <form
+          className="new-card-form__form"
+          onSubmit={this.onFormSubmit}>
+          { this.getFormFields() }
+          <div>
+            <input type="submit" value="Submit" className="new-card-form__form-button" />
+          </div>
+
+        </form>
+
+      </section>
+    );
+  }
+
+
+}
+
+const FIELDS = ['text', 'emoji'];
+
+NewCardForm.propTypes = {
+  addCardCallback: PropTypes.func,
+};
+
+export default NewCardForm;
